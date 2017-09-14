f892c7f1fdb51b686dc7831061faa047528be08b
JH Kuipers <jd@leetbook>
2017-09-14 13:04:39 +0200

    packet_aggregator: added configurable port

--- packet_aggregator.py
+++ packet_aggregator.py
@@ -13,6 +13,27 @@ def cli():
 def run():
     aggregate(interval=300)
 
+@click.command()
+@click.option('--host', default='localhost')
+@click.option('--port', default=6379)
+def print(host, port):
+    r = redis.StrictRedis(host=host, port=port, db=0)
+    p = r.pubsub()
+    p.subscribe('packets')
+    try:
+        # non-blocking
+        # while True:
+        #     print(p.get_message())
+
+        # blocking
+        start_interval = time.time()
+        for message in p.listen():
+            if isinstance(message['data'], bytes):
+                payload = msgpack.unpackb(message['data'], encoding='utf-8')
+                print(payload)
+    except KeyboardInterrupt as e:
+        print("Keyboard interrupt")
+        pass
 
 def aggregate(interval=300):
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
