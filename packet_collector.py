4158ed02a6999c5aad0bca8a3bc4ef5d15135760
JH Kuipers <jd@leetbook>
2017-09-14 07:35:14 +0200

    packet_collector: now outputs all required data

--- packet_collector.py
+++ packet_collector.py
@@ -15,37 +15,69 @@ def stream_packets():
     #pc.setfilter('udp dst port 53')
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     p = r.pubsub()
+
+
     try:
         for ts, pkt in pc:
             eth = dpkt.ethernet.Ethernet(pkt)
+            data = {}
             ip = eth.data
-            proto = ''
-            plen = 0
-            ip_version = ''
-            ip_src = ''
-            ip_dst = ''
-            if eth.type == 2048:
-                ip_version = 'ipv4'
-                proto = ip.p
-                plen = ip.len
-                ip_src = socket.inet_ntoa(ip.src)  # 5
-                ip_dst = socket.inet_ntoa(ip.dst)
-            elif eth.type == 34525:
-                ip_version = 'ipv6'
-                proto = ip.nxt
-                ip_src = socket.inet_ntop(socket.AF_INET6, ip.src)  # 5
-                ip_dst = socket.inet_ntop(socket.AF_INET6, ip.dst)
-                #NOTE the non-fixed IPv6 headers should be subtracted from this value to get the actual payload size
-                plen = ip.plen
+            if eth.type == 2048: #only process IPv4
+                data['ip_ttl'] = ip.ttl
+                data['ip_proto'] = ip.p
+                data['ip_length'] = ip.len
+                data['ip_src'] = socket.inet_ntoa(ip.src)  # 5
+                data['ip_dst'] = socket.inet_ntoa(ip.dst)
+
+                if (ip.p == 6 or ip.p == 17):  # Only add data for TCP or UDP
+                    proto = ip.data
+                    data['sport'] = proto.sport  # 7
+                    data['dport'] = proto.dport  # 8
+                    tcp_flag = ""
+                    if ip.p == 6:
+                        try:
+                            tcp_flag += ("F" if (int(proto.flags & dpkt.tcp.TH_FIN) != 0) else ".")  # 27
+                            tcp_flag += ("S" if (int(proto.flags & dpkt.tcp.TH_SYN) != 0) else ".")  # 26
+                            tcp_flag += ("R" if (int(proto.flags & dpkt.tcp.TH_RST) != 0) else ".")  # 25
+                            tcp_flag += ("P" if (int(proto.flags & dpkt.tcp.TH_PUSH) != 0) else ".")  # 24
+                            tcp_flag += ("A" if (int(proto.flags & dpkt.tcp.TH_ACK) != 0) else ".")  # 23
+                            tcp_flag += ("U" if (int(proto.flags & dpkt.tcp.TH_URG) != 0) else ".")  # 22
+                            tcp_flag += ("E" if (int(proto.flags & dpkt.tcp.TH_ECE) != 0) else ".")  # 21
+                            tcp_flag += ("C" if (int(proto.flags & dpkt.tcp.TH_CWR) != 0) else ".")  # 20
+                        except:
+                           pass
+                        data['tcp_flag'] = tcp_flag
+                        if (proto.dport == 80) or (proto.dport == 443):
+                            http_request_method = ''
+                            if not proto.data == '':
+                                try:
+                                    http_request_method = dpkt.http.Request(proto.data).method
+                                except:
+                                    http_request_method = ''
+                            data['http_data'] = http_request_method
+
+                    fragments = 1 if (
+                        int(
+                            ip.off & dpkt.ip.IP_MF) != 0) else 0  # 8 This flag is set to a 1 for all fragments except the last one
+                    data['fragments'] = fragments
+
+                # elif eth.type == 34525:
+                #     ip_version = 'ipv6'
+                #     proto = ip.nxt
+                #     ip_src = socket.inet_ntop(socket.AF_INET6, ip.src)  # 5
+                #     ip_dst = socket.inet_ntop(socket.AF_INET6, ip.dst)
+                #     #NOTE the non-fixed IPv6 headers should be subtracted from this value to get the actual payload size
+                #     plen = ip.plen
+
+                # if proto == 6:
+                #     proto ='tcp'
+                # elif proto == 17:
+                #     proto = 'udp'
+
+                r.publish('packets', msgpack.packb(data))
             else:
                 continue
 
-            if proto == 6:
-                proto ='tcp'
-            elif proto == 17:
-                proto = 'udp'
-
-            r.publish('test', msgpack.packb({'ip_version': ip_version, 'proto' : proto, 'length' : plen, 'src_ip': ip_src, 'dst_ip' : ip_dst}))
     except KeyboardInterrupt as e:
         print("Keyboard interrupt")
         pass
