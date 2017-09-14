043966dd56a5d30e00d57a56d1b6a7db9a7f6e96
JH Kuipers <jd@leetbook>
2017-09-14 14:08:40 +0200

    packet_aggregator: added time limiter

--- packet_aggregator.py
+++ packet_aggregator.py
@@ -39,10 +39,11 @@ def print_packets(host, port):
         pass
 @click.command()
 @click.option('--interval', default=300)
+@click.option('--time_limit', default=300)
 @click.option('--packets', default=-1, help='the maximum number of packets to aggregate before starting analysis, set to -1 to ignore packet limit, defaults to -1')
 @click.option('--host', default='localhost')
 @click.option('--port', default=6379)
-def aggregate(interval=300, packets=5000, host='localhost', port=6379):
+def aggregate(interval=300, time_limit=300, packets=5000, host='localhost', port=6379):
     r = redis.StrictRedis(host=host, port=port, db=0)
     p = r.pubsub()
     p.subscribe('packets')
@@ -81,13 +82,16 @@ def aggregate(interval=300, packets=5000, host='localhost', port=6379):
         #     print(p.get_message())
 
         # blocking
-        start_interval = time.time()
+        start_window = time.time()
+        start_timestamp = None
         current_data = []
         packet_counter = 0
         print("starting loop")
         for message in p.listen():
             if isinstance(message['data'], bytes):
                 p = msgpack.unpackb(message['data'], encoding='utf-8')
+                if start_timestamp is None:
+                    start_timestamp = p['timestamp']
                 payload = {}
                 payload.update(packet_fields)
                 payload.update(p)
@@ -101,19 +105,25 @@ def aggregate(interval=300, packets=5000, host='localhost', port=6379):
                     payload['http_data'], payload['raw_size']
                     ))
                 packet_counter += 1
-            if (packets != -1 and packet_counter >= packets) or time.time() - start_interval > interval:
-                # new set
-                reason = ''
-                if packet_counter == packets:
-                    reason = 'packet limit of {} packets hit'.format(packets)
-                else:
-                    reason = 'time limit of {} seconds reached'
-                print("Calling analyze, {}".format(reason))
+                if (packets != -1 and packet_counter >= packets) \
+                    or (time_limit != 1 and time.time() - start_window >= time_limit)\
+                    or (payload['timestamp'] - start_timestamp >= interval):
 
-                analyse(pd.DataFrame(current_data, columns=columns), 'test')
-                packet_counter = 0
-                start_interval = time.time()
-                current_data = []
+                    # new set
+                    reason = ''
+                    if packet_counter == packets:
+                        reason = 'packet limit of {} packets hit'.format(packets)
+                    elif payload['timestamp'] - start_timestamp == interval:
+                        reason = 'interval of {} seconds exceeded'.format(interval)
+                    else:
+                        reason = 'time limit of {} seconds reached'.format(time_limit)
+                    print("Calling analyze, {}".format(reason))
+
+                    analyse(pd.DataFrame(current_data, columns=columns), 'test')
+                    packet_counter = 0
+                    start_window = time.time()
+                    start_timestamp = p['timestamp']
+                    current_data = []
 
     except KeyboardInterrupt as e:
         print("Keyboard interrupt")
